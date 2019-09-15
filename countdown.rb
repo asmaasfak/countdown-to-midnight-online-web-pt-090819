72caba1eb327e78ef37ace1d44497ec7a564232a
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-15 00:10:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,17 @@
 #write your code here
 
-def countdown (num)
-  until num == 0
+def countdown (number)
+  until number == 0
   p "#{number} SECOND(S)!"
-  num -= 1
+  number -= 1
 end
 p "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (num)
-  until num == 0
+def countdown_with_sleep (number)
+  until number == 0
   puts "#{number} SECOND(S)!"
-  num -= 1
+  number -= 1
   sleep 1
 end
 #p "HAPPY NEW YEAR!"
