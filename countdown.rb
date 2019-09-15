e457a2e2ec0284f573c4ad79392b5f9e9b099332
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-15 00:07:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,11 @@ end
 p "HAPPY NEW YEAR!"
 end
 
-def countdown (num)
+def countdown_with_sleep (num)
   until num == 0
   puts "#{number} SECOND(S)!"
   num -= 1
+  sleep 1
 end
 p "HAPPY NEW YEAR!"
 end
